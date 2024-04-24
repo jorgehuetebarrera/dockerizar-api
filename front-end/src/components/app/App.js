241369766faf1ca8585f9d1e9cf48533626e2018
jorgehuetebarrera <116200501+jorgehuetebarrera@users.noreply.github.com>
2024-04-24 10:20:16 +0200

    funcionalidades completadas, login redirige al juego

--- front-end/src/components/app/App.js
+++ front-end/src/components/app/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import ArbolDecisiones from './ArbolDecisiones.js';
+import React, { useState } from 'react';
+import LoginForm from './logingForm';
+import ArbolDecisiones from './ArbolDecisiones';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userToken, setUserToken] = useState(null); // Opcional: almacenar el token del usuario
+
+  const handleLoginSuccess = (token) => {
+    setIsLoggedIn(true);
+    setUserToken(token); // Opcional: almacenar el token del usuario en el estado
+  };
+
   return (
     <div className="App">
       <h1>Sombras de la Noche</h1>
-      <ArbolDecisiones />
+      {!isLoggedIn ? (
+        <LoginForm onLoginSuccess={handleLoginSuccess} />
+      ) : (
+        <ArbolDecisiones />
+      )}
     </div>
   );
 }
