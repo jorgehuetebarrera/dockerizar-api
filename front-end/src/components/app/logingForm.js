61f0077b36372bc85a5724d98025fc84672d7af1
jorgehuetebarrera <116200501+jorgehuetebarrera@users.noreply.github.com>
2024-06-13 17:33:55 +0200

    Update logingForm.js

--- front-end/src/components/app/logingForm.js
+++ front-end/src/components/app/logingForm.js
@@ -10,7 +10,7 @@ const LoginForm = ({ onLoginSuccess }) => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:3000/user/login', {
+      const response = await fetch('https://nightinarcadia.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
