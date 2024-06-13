de423e2c74b234f1a2509b99f1ec6f5e36e296fe
jorgehuetebarrera <116200501+jorgehuetebarrera@users.noreply.github.com>
2024-06-13 17:33:38 +0200

    Update CreateUser.js

--- front-end/src/components/app/CreateUser.js
+++ front-end/src/components/app/CreateUser.js
@@ -19,7 +19,7 @@ const CreateUser = ({ onRegisterSuccess }) => {
     };
 
     try {
-      const response = await fetch('http://localhost:3000/user/register', {
+      const response = await fetch('https://nightinarcadia.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
