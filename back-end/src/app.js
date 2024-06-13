f7cdbe7f095b41ee6b7598e071964bd37bc9e108
jorgehuetebarrera <116200501+jorgehuetebarrera@users.noreply.github.com>
2024-06-13 20:48:02 +0200

    problemas

--- back-end/src/app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { init } from './loaders/index.js';
-import config from './config.js';
-
-const app = express();
-
-init(app, config);
-
-export default app;
--- back-end/src/utils/index.js
+++
@@ -1,3 +0,0 @@
-import logger from './logger.js';
-
-export default logger;
